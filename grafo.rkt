8a58fe9367f3eca4aea309bedb0862696d3fc818
FelipeJU22 <112335803+FelipeJU22@users.noreply.github.com>
2023-09-11 21:04:41 -0600

    Se crea la función de encontrar rutas

--- grafo.rkt
+++ grafo.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
-(provide agregar_arista mostrar-grafo)
+(provide agregar_arista mostrar-grafo todas_rutas)
+
+
 
 ;Funcion que agrega un nodo junto con el destino y el peso al grafo
 (define (agregar_arista grafo origen destino peso)
@@ -77,4 +79,66 @@
   
   )
 
-(main)
+
+;FUNCIONES PARA ENCONTRAR RUTAS
+
+
+;invertir un nodo y sus elementos
+(define (invertir invertirAux rutas)
+  (cond ( (null? rutas) invertirAux)
+        (else
+         (invertir (append (list (reverse (car rutas))) invertirAux)(cdr rutas)))))
+
+;Indicar los nodos adyacentes de un nodo
+(define (adyacente nodo grafo)
+  (adyacente_aux (assoc nodo grafo) nodo grafo))  ;Assoc retorna la lista que cumpla que el car, es igual al elemento dado
+
+(define (adyacente_aux comprobar nodo grafo) ;
+  (cond((equal? comprobar  #f)#f)  
+       (else(cond ((null? (cdr comprobar))
+               cdr comprobar)
+              (else (cadr comprobar))))))
+
+;comprueba si un nodo está en la lista de destinos de un nodo. False si no, True si sí
+(define (comprobar_nodo_ruta nodo lista)
+  (cond ((null? lista) #f)
+        ((equal? nodo (car lista))#t)
+        (else
+         (comprobar_nodo_ruta nodo (cdr lista)))))
+
+;Retorna las relaciones de los nodos con la forma (destino origen), para ayudar a la función de ecnontrar todas las rutas posibles
+(define (nodos_vecinos ruta grafo)
+  (nodos_vecinos_aux ruta '() grafo (adyacente (car ruta) grafo))
+  )
+(define (nodos_vecinos_aux ruta rutaGenerada grafo adyacente)
+  (cond ((null? adyacente) rutaGenerada)
+        (else
+         (cond ((comprobar_nodo_ruta (caar adyacente) ruta)
+                (nodos_vecinos_aux ruta rutaGenerada grafo (cdr adyacente)))
+               (else 
+                 (nodos_vecinos_aux ruta(append (list (cons (caar adyacente) ruta)) rutaGenerada) grafo (cdr adyacente))))
+       )
+     )
+  )
+
+
+;Comprueba si en un recorrido, se llegó del nodo origen, al destino
+(define (comprobar_destino destino recorrido)
+  (equal? destino (car recorrido)))
+
+;retornar todas las rutas posibles de un punto a, hacia un punto b
+(define (todas_rutas origen destino g)
+  (todas_rutas_aux (list (list origen)) destino g '()))
+(define (todas_rutas_aux rutas destino grafo final)
+  (cond ((null? rutas)
+         (invertir '() final))
+        ((comprobar_destino destino (car rutas))
+         (todas_rutas_aux (cdr rutas) destino grafo (cons (car rutas) final) ))
+        (else
+         (todas_rutas_aux
+          (append (nodos_vecinos (car rutas) grafo) (cdr rutas)) destino grafo final))))
+(displayln (todas_rutas 'A 'B '((C((E 4)))(B ((C 2)))(A ((B 2)(E 6))) (E((A 1))))))
+
+
+
+;(main)
